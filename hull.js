1718568bba859675f553503280f36fc3bfc5d0bb
abhiraj-mengade <i2am@yandex.com>
2022-09-30 09:49:41 +0530

    updates

---
+++ hull.js
@@ -0,0 +1,153 @@
+var pointRadius = 1;
+var hullPadding = 5; 
+         //                 });
+var vecFrom = function (p0, p1) {               // Vector from p0 to p1
+    return [ p1[0] - p0[0], p1[1] - p0[1] ];
+}
+
+var vecScale = function (v, scale) {            // Vector v scaled by 'scale'
+    return [ scale * v[0], scale * v[1] ];
+}
+
+var vecSum = function (pv1, pv2) {              // The sum of two points/vectors
+    return [ pv1[0] + pv2[0], pv1[1] + pv2[1] ];
+}
+
+var vecUnit = function (v) {                    // Vector with direction of v and length 1
+    var norm = Math.sqrt (v[0]*v[0] + v[1]*v[1]);
+    return vecScale (v, 1/norm);
+}
+
+var vecScaleTo = function (v, length) {         // Vector with direction of v with specified length
+    return vecScale (vecUnit(v), length);
+}
+
+var unitNormal = function (pv0, p1) {           // Unit normal to vector pv0, or line segment from p0 to p1
+    if (p1 != null) pv0 = vecFrom (pv0, p1);
+    var normalVec = [ -pv0[1], pv0[0] ];
+    return vecUnit (normalVec);
+};
+
+
+// Hull Generators
+
+var lineFn = d3.line()
+    .curve (d3.curveCatmullRomClosed)
+    .x (function(d) { return d.p[0]; })
+    .y (function(d) { return d.p[1]; });
+
+
+var smoothHull = function (polyPoints) {
+    // Returns the SVG path data string representing the polygon, expanded and smoothed.
+
+    var pointCount = polyPoints.length;
+
+    // Handle special cases
+    if (!polyPoints || pointCount < 1) return "";
+    if (pointCount === 1) return smoothHull1 (polyPoints);
+    if (pointCount === 2) return smoothHull2 (polyPoints);
+
+    var hullPoints = polyPoints.map (function (point, index) {
+        var pNext = polyPoints [(index + 1) % pointCount];
+        return {
+            p: point,
+            v: vecUnit (vecFrom (point, pNext))
+        };
+    });
+
+    // Compute the expanded hull points, and the nearest prior control point for each.
+    for (var i = 0;  i < hullPoints.length;  ++i) {
+        var priorIndex = (i > 0) ? (i-1) : (pointCount - 1);
+        var extensionVec = vecUnit (vecSum (hullPoints[priorIndex].v, vecScale (hullPoints[i].v, -1)));
+        hullPoints[i].p = vecSum (hullPoints[i].p, vecScale (extensionVec, hullPadding));
+    }
+
+    return hullPoints;
+}
+
+function polarAngle(a, b, c) {
+    let x = (a[0] - b[0]) * (c[0] - b[0]) + (a[1] - b[1]) * (c[1] - b[1]);
+    let y = (a[0] - b[0]) * (c[1] - b[1]) - (c[0] - b[0]) * (a[1] - b[1]);
+    return Math.atan2(y, x);
+}
+
+function convexHull(p_list) {
+    if (p_list.length < 3) return p_list;
+
+    let hull = [];
+    let tmp;
+
+    // Find leftmost point
+    tmp = p_list[0];
+    for (const p of p_list) if (p[0] < tmp[0]) tmp = p;
+
+    hull[0] = tmp;
+
+    let endpoint, secondlast;
+    let min_angle, new_end;
+
+    endpoint = hull[0];
+    secondlast = [endpoint[0], endpoint[1]+10];
+
+    do {
+        min_angle = Math.PI; // Initial value. Any angle must be lower that 2PI
+        for (const p of p_list) {
+            tmp = polarAngle(secondlast, endpoint, p);
+
+            if (tmp <= min_angle) {
+                new_end = p;
+                min_angle = tmp;
+            }
+        }
+
+        if (new_end != hull[0]) {
+            hull.push(new_end);
+            secondlast = endpoint;
+            endpoint = new_end;
+        }
+    } while (new_end != hull[0]);
+    hull.push(hull[0]);
+    // hull = catmullrom(hull);
+
+    return hull ;
+}
+
+function catmullrom(ddata){
+    if(ddata.length<3){
+        return ddata
+    }
+    var data =[];
+    for(var i=0;i<ddata.length-2;i++){
+        let p1 = ddata[i]
+        let p2 = ddata[(i+1)%ddata.length]
+        let p3 = ddata[(i+2)%ddata.length]
+        let p4 = ddata[(i+3)%ddata.length]
+
+        let d1 = Math.sqrt((p2[0]-p1[0])**2+(p2[1]-p1[1])**2)
+        let d2 = Math.sqrt((p3[0]-p2[0])**2+(p3[1]-p2[1])**2)
+        let d3 = Math.sqrt((p4[0]-p3[0])**2+(p4[1]-p3[1])**2)
+
+        let v1 = [p2[0]-p1[0],p2[1]-p1[1]]
+        let v2 = [p3[0]-p2[0],p3[1]-p2[1]]
+        let v3 = [p4[0]-p3[0],p4[1]-p3[1]]
+
+        let v1n = [v1[0]/d1,v1[1]/d1]
+        let v2n = [v2[0]/d2,v2[1]/d2]
+        let v3n = [v3[0]/d3,v3[1]/d3]
+
+        let v1n2 = [v1n[0]+v2n[0],v1n[1]+v2n[1]]
+        let v2n2 = [v2n[0]+v3n[0],v2n[1]+v3n[1]]
+        
+        for(var j=0;j<100;j++){
+            let t = j/100
+            let x = (2*t**3-3*t**2+1)*p2[0]+(-2*t**3+3*t**2)*p3[0]+(t**3-2*t**2+t)*d2*v1n2[0]+(t**3-t**2)*d3*v2n2[0]
+            let y = (2*t**3-3*t**2+1)*p2[1]+(-2*t**3+3*t**2)*p3[1]+(t**3-2*t**2+t)*d2*v1n2[1]+(t**3-t**2)*d3*v2n2[1]
+            data.push([x,y])
+        }
+
+    }
+
+        return data;
+
+
+    }
---
+++ script.js
@@ -0,0 +1,250 @@
+
+var colordic={};
+for(var i = 0; i <100; i++) {
+    colordic[Highcharts.color(Highcharts.getOptions()
+        .colors[i]).setOpacity(0.5).get()] = i;
+}
+
+
+function getn(n) {
+    return Math.random() * n;
+  }
+var n =10;
+alld=[]
+djs=[]
+for(var i =8; i>=0;i--)
+{
+    var data = new Array((i+1)*20).fill().map((_) => {return [getn((i+2)*40),getn((i+2)*20*(i+1))]});
+    djs.push(data);
+
+}
+
+var datajs =[{
+    'id': '1',
+    name: 'Material 1',
+    'data': djs[0],
+    'hasdepth':false
+},
+{   'id': '2',
+    name: 'Material 2',
+    'data': [
+        {
+            'id': '3',
+            'name': 'Material 2.1',
+            'data': djs[1],
+            'hasdepth':false
+        },
+        {   'id': '4',
+            'name': 'Material 2.2',
+            'data': djs[2],
+            'hasdepth':false
+        }
+
+    ],
+    'hasdepth':true
+},
+{   'id': '5',
+    name: 'Material 3',
+    'data': djs[3],
+    'hasdepth':false
+},
+{   'id': '6',
+    name: 'Material 4',
+    'data': [
+        {
+            'id': '7',
+            'name': 'Material 4.1',
+            'data': djs[4],
+            'hasdepth':false
+        },
+        {   'id': '8',
+            'name': 'Material 4.2',
+            'data': [
+                {
+                    'id': '9',
+                    'name': 'Material 4.2.1',
+                    'data': djs[5],
+                    'hasdepth':false
+                },
+                {   'id': '10',
+                    'name': 'Material 4.2.2',
+                    'data': djs[6],
+                    'hasdepth':false
+                },
+                {   'id': '11',
+                    'name': 'Material 4.2.3',
+                    'data': djs[7],
+                    'hasdepth':false
+                },
+
+            ],
+            'hasdepth':true
+        },
+    ],
+    'hasdepth':true
+},
+]
+
+
+
+var expanded={}; 
+var parenttree={};
+var childrentree={};
+var cur=0;
+
+
+
+function dfs(key){
+    expanded[key.id] = {};
+    if(key.hasdepth){
+        var tempdata=[];
+        childrentree[key.id]=[];
+        for(var i =0;i<key.data.length;i++){
+            tempdata.push(...dfs(key.data[i]));
+            childrentree[key.id].push(key.data[i].id);
+            parenttree[key.data[i].id]=key.id;
+
+        }
+        tempdata=tempdata;
+        expanded[key.id].data=tempdata;
+        expanded[key.id].name=key.name;
+        return tempdata;
+    }
+    else{
+        var data = key.data;
+        data = data;
+        expanded[key.id].data=data;
+        expanded[key.id].name=key.name;
+        return data;
+    }
+}
+
+
+
+for(var i =0; i<datajs.length;i++)
+{
+    dfs(datajs[i]);
+}
+
+
+
+function formatter(key){
+    var s = {
+        name: expanded[key].name,
+        type: 'polygon',
+        marker: {
+            symbol: 'square'
+        },
+        data: convexHull(expanded[key].data),
+        color: Highcharts.color(Highcharts.getOptions()
+            .colors[key]).setOpacity(0.5).get(),
+        // enableMouseTracking: false,
+        // allowPointSelect:true,
+        // accessibility: {
+        //     exposeAsGroupOnly: false,
+        //     description: 'Material'
+        // }
+}
+return s;
+}
+
+function displaydata(key){
+    let ddata =[]
+    cur = key;
+    if(childrentree[key]){
+        for(var i =0; i<childrentree[key].length;i++){
+            ddata.push(formatter(childrentree[key][i]));
+        }
+    }
+    else{
+        ddata.push(formatter(key));
+    }
+    
+
+
+    for (var i = alld.length-1; i >=0; i--) {
+        chart.series[i].remove();  
+    }
+    ddata= ddata.sort()
+    alld=ddata;
+    console.log(ddata);
+    for (var i = 0; i < ddata.length; i++) {
+        chart.addSeries(ddata[i]);  
+    }
+    chart.redraw();
+    
+
+}
+
+
+
+function getdata(){
+    return alld;
+}
+childrentree[0]=[];
+for(var i =0;i<datajs.length;i++){
+    alld.push(formatter(datajs[i].id));
+    childrentree[0].push(datajs[i].id);
+}
+// datajs =[{
+//     "id":1,
+//     "name": "M1",
+//     "data":[[1,2],[0,1]],
+//     "parent":null,
+//     "children":[2,3]
+// },
+// {
+//     "id":2,
+//     "name": "M2",
+//     "data": null,
+//     "parent":null
+// },
+// {
+//     "id":3,
+//     "name": "M2.1",
+//     "data":[[2,3],[3,4]],
+//     "parent":2,
+// },{
+//     "id":4,
+//     "name": "M2.2",
+//     "data":[[4,5],[5,6]],
+//     "parent": 2
+// }
+// ]
+
+// datajs = {
+//     1: {
+//         "name": "M1",
+//         "data":[[1,2],[0,1]],
+//         "parent": null
+//     },
+//     2: {
+//         "name": "M2",
+//         "data": null,
+//         "parent": null
+//     },
+//     3: {
+//         "name": "M2.1",
+//         "data":[[2,3],[3,4]],
+//         "parent": 2
+//     },
+//     4: {
+//         "name": "M2.2",
+//         "data":[[4,5],[5,6]],
+//         "parent": 2
+//     }
+// }
+
+// for(var i=0;i<datajs.length;i++)
+// {
+//     var data = datajs[i];
+//     if(data.hasdepth)
+//     {
+//         for(var j=0;j<data.data.length;j++)
+//         {
+//             var data2 = data.data[j];
+//             alld.push(data2);
+//         }
+//     }
+
+// }
